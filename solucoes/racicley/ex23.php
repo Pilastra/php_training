f9d1b7bf490804210abf0b59e8df32d7b82f3eea
Rafael Kaustchr <rkaustchr@hotmail.com>
2017-01-10 19:19:26 -0200

    Correcoes e comentarios

--- solucoes/racicley/ex23.php
+++ solucoes/racicley/ex23.php
@@ -6,4 +6,16 @@ include 'ex21.php';
 echo '<pre>';
 print_r(conta_letra('ratazana verde'));
 echo '</pre>';
+
+
+/*
+	Pegou a diferença entre include e require ? e as variações _once ?
+
+	include : se der erro ao incluir o arquivo o programa continua (warning)
+	require : se der erro ao incluir o arquivo o programa aborta (error)
+
+	include_once, require_once : Só inclui o arquivo uma vez, se o arquivo já foi incluso ele ignora e não duplica a inclusão.
+
+*/
+
 ?>
